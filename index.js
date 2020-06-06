6384bffa3eb18aa28dbf95f0986b6ffd7a11da92
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 19:24:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -156,9 +156,9 @@ function moveDodgerRight() {
 function start() {
   window.addEventListener('keydown', moveDodger)
   START.style.display = 'none'
-  // gameInterval = setInterval(function() {
-  //   createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
-  // }, 1000)
+  gameInterval = setInterval(function() {
+    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
+  }, 1000)
 }
 
 document.querySelector('#start').addEventListener('click', function(e) {

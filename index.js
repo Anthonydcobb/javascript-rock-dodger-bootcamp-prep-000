ae823997431629123216c6c4108bd5e0cdb67bc8
Anthony David Cobb <anthonydcobb@gmail.com>
2020-06-06 19:25:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -66,30 +66,30 @@ function createRock(x) {
    * seems like a good pace.)
    */
 //   function moveRock() {
-// 
+//
 //     }
-// 
+//
 //     //  * If a rock collides with the DODGER,
 //     //  * we should call endGame().
 //     //  *
-// 
+//
 //     /**
 //      * Otherwise, if the rock hasn't reached the bottom of
 //      * the GAME, we want to move it again.
 //      */
-// 
+//
 //     /**
 //      * But if the rock *has* reached the bottom of the GAME,
 //      * we should remove the rock from the DOM.
 //      */
 //   }
-// 
+//
 //   // We should kick off the animation of the rock around here.
-// 
+//
 //   // Add the rock to ROCKS so that we can remove all rocks
 //   // when there's a collision.
 //   ROCKS.push(rock)
-// 
+//
 //   // Finally, return the rock element you've created.
 //   return rock
 // }
